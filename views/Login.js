faf4cc2a583b045e30f8c28fe7438d02fc721d21
shaynek <shayne.kandagor@metropolia.fi>
2023-01-16 10:53:36 +0200

    dummy login done

--- views/Login.js
+++ views/Login.js
@@ -1,14 +1,37 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import {StyleSheet, View, Text, Button} from 'react-native';
 import PropTypes from 'prop-types';
 import {MainContext} from '../contexts/MainContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Login = ({navigation}) => {
   const {setIsLoggedIn} = useContext(MainContext);
-  const logIn = () => {
+
+  const logIn = async () => {
     console.log('Login button pressed');
     setIsLoggedIn(true);
+    try {
+      await AsyncStorage.setItem('userToken', 'abc123');
+    } catch (error) {
+      console.warn('error in storing token', error);
+    }
   };
+
+  const checkToken = async () => {
+    try {
+      const userToken = await AsyncStorage.getItem('userToken');
+      if (userToken === 'abc123') {
+        setIsLoggedIn(true);
+      }
+    } catch (error) {
+      console.log('no valid token available');
+    }
+  };
+
+  useEffect(() => {
+    checkToken();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Login</Text>
--- views/Profile.js
+++ views/Profile.js
@@ -1,6 +1,7 @@
 import React, {useContext} from 'react';
 import {StyleSheet, SafeAreaView, Text, Button} from 'react-native';
 import {MainContext} from '../contexts/MainContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Profile = () => {
   const {setIsLoggedIn} = useContext(MainContext);
@@ -9,9 +10,14 @@ const Profile = () => {
       <Text>Profile</Text>
       <Button
         title="Logout!"
-        onPress={() => {
-          console.log('Loggin Out!');
+        onPress={async () => {
+          console.log('Logging Out!');
           setIsLoggedIn(false);
+          try {
+            await AsyncStorage.clear();
+          } catch (error) {
+            console.error('clearing asyncstorage failed', error);
+          }
         }}
       />
     </SafeAreaView>

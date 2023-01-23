6aab813d5bf22073ca7fc025e6ce284cba83de46
Shayne kandagor <shayne.kandagor@gmail.com>
2023-01-23 09:52:39 +0200

    avatar added

--- views/Profile.js
+++ views/Profile.js
@@ -1,13 +1,31 @@
-import React, {useContext} from 'react';
+import React, {lazy, useContext, useEffect, useState} from 'react';
 import {StyleSheet, SafeAreaView, Text, Button} from 'react-native';
 import {MainContext} from '../contexts/MainContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useTag} from '../hooks/ApiHooks';
 
 const Profile = () => {
+  const {getFilesByTag} = useTag();
   const {setIsLoggedIn} = useContext(MainContext);
+  const [avatar, setAvatar] =  useState('');
+
+  const loadAvatar = async () => {
+    const avatar = await getFilesByTag('avatar_' + user.user_id);
+    setAvatar(avatarArray.pop().filename);
+    console.log('user avatar', avatar);
+  };
+
+  useEffect(() => {
+    loadAvatar();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Profile</Text>
+      <Image style={styles.image} source={{uri: uploadsUrl + avatar}} />
+      <Text>Username: {user.name}</Text>
+      <Text>Email: {user.email}</Text>
+      <Text>Full name: {user.full_name}</Text>
       <Button
         title="Logout!"
         onPress={async () => {
@@ -31,6 +49,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     paddingTop: 40,
+  },
+  image: {
+    width: 200,
+    height: 300,
   },
 });
 
